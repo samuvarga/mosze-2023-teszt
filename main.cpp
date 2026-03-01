7ba4737866b2c89c0b3ce2638ff983575fb4ef5e
Varga Sámuel <samuvarga24@gmail.com>
2026-03-01 11:57:03 +0100

    Hibák javítása

--- main.cpp
+++ main.cpp
@@ -4,32 +4,26 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    // HIBA: hibás konstans név, NELEMENTS helyett N_ELEMENTS kellene
-    int *b = new int[NELEMENTS];
-    // HIBA: karakter literal használt, hiányzik a pontosvessző
-    std::cout << '1-100 ertekek duplazasa'
-    // HIBA: for fej hiányos (feltétel, növelés, i deklaráció)
-    for (int i = 0;)
+    int *b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
+    for (int i = 0; i < N_ELEMENTS; ++i)
     {
-        // HIBA: a ciklus törzse nem módosítja i-t, végtelen ciklus
         b[i] = i * 2;
     }
-    // HIBA: feltétel i; sosem fut, 0 kezdőérték miatt
-    for (int i = 0; i; i++)
+
+    for (int i = 0; i < N_ELEMENTS; ++i)
     {
-        // HIBA: hiányzó kiírandó érték és pontosvessző
-        std::cout << "Ertek:"
-    }    
+        std::cout << "Ertek: " << b[i] << std::endl;
+    }
+
     std::cout << "Atlag szamitasa: " << std::endl;
-    // HIBA: atlag nincs inicializálva
-    int atlag;
-    // HIBA: for fejben vessző, növelés hiányzik
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag = 0;
+    for (int i = 0; i < N_ELEMENTS; ++i)
     {
-        // HIBA: pontosvessző hiányzik
-        atlag += b[i]
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    delete[] b;
     return 0;
 }
